dd7666a7e11fe78a541fea50c8ce3488fe30e9d0
ATTM-Blog <accordingtothemodel@gmail.com>
2025-06-30 00:47:27 +0100

    Update site.js

--- site.js
+++ site.js
@@ -1,13 +1,13 @@
 
 const BLOG_TITLE = "According to the Model";
-const COPYRIGHT_NOTICE = "© 2025 According to the Model";
+const COPYRIGHT_NOTICE = "© 2025 According to the Model Blog";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.title = BLOG_TITLE;
   const header = document.querySelector("header");
   if (header) {
     header.innerHTML = `
-      <img src="logo.png" alt="Logo" style="height: 120px;">
+      <img src="logo.png" alt="Logo" style="height: 160px;">
       <h1>${BLOG_TITLE}</h1>
       <nav>
         <a href="index.html">Home</a>
